da650b8ed721c8c153378e153953c547226ffca1
krish9495 <149101869+krish9495@users.noreply.github.com>
2024-07-24 15:37:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const dynamicText = document.querySelector(".dynamic-text");
+const words = ["Web Developer","UI/UX Designer","Software Developer", "Video Editor", "Cinematographer"];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    if (!isDeleting) {
+        dynamicText.textContent = currentWord.substring(0, charIndex);
+        charIndex++;
+        if (charIndex === currentWord.length) {
+            setTimeout(() => { isDeleting = true; }, 2000); // Pause before deleting
+        }
+    } else {
+        dynamicText.textContent = currentWord.substring(0, charIndex);
+        charIndex--;
+        if (charIndex === 0) {
+            isDeleting = false;
+            wordIndex = (wordIndex + 1) % words.length;
+        }
+    }
+    setTimeout(typeEffect, isDeleting ? 100 : 200);
+}
+
+typeEffect();
+
+document.querySelectorAll('.navbar li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
